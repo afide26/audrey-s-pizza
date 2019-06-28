e2aef5118b320b613a2b6953f49766303f59cea2
Alan Fidelino <afidedev26@gmail.com>
2019-06-28 18:58:49 +1000

    Add food category

--- src/Data/FoodData.js
+++ src/Data/FoodData.js
@@ -1,35 +1,43 @@
 const foodsAndDrinks = [{
     name: 'Audrey\'s Special',
-    img: '/img/pizza.jpg'
+    img: '/img/pizza.jpg',
+    category: 'pizza'
   },
   {
     name: 'Margeherita',
-    img: '/img/margherita.jpg'
+    img: '/img/margherita.jpg',
+    category: 'pizza'
   },
   {
     name: 'Carl\'s Burger',
-    img: '/img/burger.jpg'
+    img: '/img/burger.jpg',
+    category: 'sandwiches'
   },
   {
     name: 'Carl\'s Calzone',
-    img: '/img/calzone.jpg'
+    img: '/img/calzone.jpg',
+    category: 'sandwiches'
   },
   {
     name: 'Tin\'s Fries',
-    img: '/img/fries.jpg'
+    img: '/img/fries.jpg',
+    category: 'sides'
 
   },
   {
     name: 'Family Skewers',
-    img: '/img/skewers.jpg'
+    img: '/img/skewers.jpg',
+    category: 'sides'
   },
   {
     name: 'House Cocktails',
-    img: '/img/cocktails.jpg'
+    img: '/img/cocktails.jpg',
+    category: 'drinks'
   },
   {
     name: 'Softdrinks',
-    img: '/img/softdrinks.jpg'
+    img: '/img/softdrinks.jpg',
+    category: 'drinks'
   },
 
 ]
