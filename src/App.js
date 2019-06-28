0930ffe87d387070ef608730a1d1fb9a8dbdd2a8
Alan Fidelino <afidedev26@gmail.com>
2019-06-28 17:03:44 +1000

    Add Google Fonts and styled-components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body{
+    font-family: 'Open Sans', sans-serif;
+  }
+  h1,h2,h3,h4,h5,h6{
+    font-family: 'Roboto', sans-serif;
+  }
+`;
 
 function App() {
-  return <div>Welcome to Audrey's Pizza</div>;
+  return (
+    <>
+      <GlobalStyle />
+      <div>
+        <h1>Welcome to Audrey's Pizza</h1>
+      </div>
+    </>
+  );
 }
 
 export default App;
