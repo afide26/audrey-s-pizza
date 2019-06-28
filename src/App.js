9c5baa47ded4cfbeccc8221781eb2af61d4abfc5
Alan Fidelino <afidedev26@gmail.com>
2019-06-28 17:25:59 +1000

    Navbar, Styles folder and Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { createGlobalStyle } from "styled-components";
-
+import Navbar from "./Navbar/Navbar";
 const GlobalStyle = createGlobalStyle`
   body{
     font-family: 'Open Sans', sans-serif;
+    padding:0;
+    margin:0;
   }
   h1,h2,h3,h4,h5,h6{
     font-family: 'Roboto', sans-serif;
@@ -14,6 +16,7 @@ function App() {
   return (
     <>
       <GlobalStyle />
+      <Navbar />
       <div>
         <h1>Welcome to Audrey's Pizza</h1>
       </div>
---
+++ src/Navbar/Navbar.js
@@ -0,0 +1,28 @@
+import React from "react";
+import styled from "styled-components";
+import { pizzaRed } from "../Styles/colors";
+import { Title } from "../Styles/title";
+
+export const NavbarStyled = styled.div`
+  background-color: ${pizzaRed};
+  padding: 10px;
+`;
+
+const Logo = styled(Title)`
+  font-size: 20px;
+  color: #fff;
+  text-shadow: 1px 1px 3px #58585a;
+`;
+
+export default function Navbar() {
+  return (
+    <NavbarStyled>
+      <Logo>
+        Audrey's Pizza{" "}
+        <span role="img" aria-label="pizza">
+          🍕🍕🍕
+        </span>
+      </Logo>
+    </NavbarStyled>
+  );
+}
