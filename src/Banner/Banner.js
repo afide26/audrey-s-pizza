b750ceb427cdf2cfdfc25ff0284852df5ab56786
Alan Fidelino <afidedev26@gmail.com>
2019-06-28 17:40:32 +1000

    Add CSS Filter on Banner component

--- src/Banner/Banner.js
+++ src/Banner/Banner.js
@@ -2,8 +2,8 @@ import styled from 'styled-components';
 
 export const Banner = styled.div `
 height:300px;
-border: 1px solid red;
 background: url("img/Banner.jpg");
 background-position:center;
 background-size:cover;
+filter:contrast(75%);
 `
